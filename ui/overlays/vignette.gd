8b989cff174b7369fb7dfd69ecefe1fb75c4fbf7
Kendall Roth <kendall@kendallroth.ca>
2024-03-09 13:48:27 -0500

    Fix vignette animation bug

--- ui/overlays/vignette.gd
+++ ui/overlays/vignette.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+@onready var animation_player := $AnimationPlayer
+
 
 func _ready():
     GameEvents.player_health_changed.connect(on_player_health_changed)
@@ -9,4 +11,11 @@ func on_player_health_changed(health: float, change: float, max_health: float):
     if change > 0:
         return
 
-    $AnimationPlayer.play("hit")
+    # NOTE: Workaround to ensure vignette always returns to default state (even when paused)
+    var previous_process_mode := process_mode
+    process_mode = Node.PROCESS_MODE_ALWAYS
+
+    animation_player.play("hit")
+
+    await animation_player.animation_finished
+    process_mode = previous_process_mode
